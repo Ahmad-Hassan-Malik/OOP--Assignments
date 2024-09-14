4ee200f02e9d036d638475d5c786b76d64b9035c
Ahmad-Hassan-Malik <ahmadhassan4714118@gmail.com>
2024-09-14 12:34:45 +0500

    Add files via upload

---
+++ Week1-Lab1/Circle.java
@@ -0,0 +1,16 @@
+public class Circle {
+
+	public double radius = 1.5;
+
+
+	public double calArea(){
+		return radius * radius *3.14;
+}
+
+	public double calCircumfarence(){
+		return radius1 * 3.14 *2;
+}
+
+	public static void main(String[] args) {
+}
+}
---
+++ Week1-Lab1/Rectangle.java
@@ -0,0 +1,20 @@
+public class Rectangle {
+
+	public double length = 3.5;
+	public double width = 2.3;
+
+	public double calArea() {
+		return length * width;
+}
+
+	public double calPerimeter() {
+		return 2 * (length + width);
+}
+
+	public static void main (String[] args) {
+	
+}
+
+
+
+}
